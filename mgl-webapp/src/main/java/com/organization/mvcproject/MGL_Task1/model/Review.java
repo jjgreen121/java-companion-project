2192ad67e56f604245379289daff4246b4fd665e
justin.a.green <justin.a.green@AFSWWNDOM0BW01.us.accenturefederal.com>
2021-11-24 16:01:30 -0600

    refactored variable to fit OOP conventions (reviewBody -> body) as well as associated setters and getters

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Review.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Review.java
@@ -2,11 +2,7 @@ package com.organization.mvcproject.MGL_Task1.model;
 
 public class Review {
 	
-	/**
-	 * TODO 1.0 java object member variable naming convention, 
-	 * one member declared here is not object oriented refactor it
-	 */
-	private String reviewBody;
+	private String body;
 	private String author;
 	private Integer rating;
 
@@ -26,11 +22,11 @@ public class Review {
 		this.rating = rating;
 	}
 
-	public String getReviewBody() {
-		return reviewBody;
+	public String getBody() {
+		return body;
 	}
 
-	public void setReviewBody(String reviewBody) {
-		this.reviewBody = reviewBody;
+	public void setBody(String body) {
+		this.body = body;
 	}
 }
