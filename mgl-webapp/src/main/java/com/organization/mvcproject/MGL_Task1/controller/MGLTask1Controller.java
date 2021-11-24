67af8cf1cd6ec7cafa34574439fee0103dc4d196
justin.a.green <justin.a.green@AFSWWNDOM0BW01.us.accenturefederal.com>
2021-11-24 16:14:05 -0600

    Refractored Game_Service to GameService

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/controller/MGLTask1Controller.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/controller/MGLTask1Controller.java
@@ -16,15 +16,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.organization.mvcproject.MGL_Task1.model.Game;
 import com.organization.mvcproject.MGL_Task1.model.Review;
-import com.organization.mvcproject.MGL_Task1.service.Game_Service;
+import com.organization.mvcproject.MGL_Task1.service.GameService;
 
-//TODO 1.0  follow java class naming, improve class name
 @Controller
-public class MGL_Task1_Controller {
+public class MGLTask1Controller {
 
 	//TODO 1.0 variable naming convention, improve reference name
 	@Autowired
-	private Game_Service javaGameService;
+	private GameService javaGameService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
